4e28f0978381cdb8fc7f82e85a01ed420039206a
Chris Hey <chrishey87@hotmail.com>
2014-03-03 17:11:35 +0000

    Added github repos environment variable

--- Modules/GitProjectModule/GitProjectModule.psm1
+++ Modules/GitProjectModule/GitProjectModule.psm1
@@ -1,5 +1,11 @@
 ﻿function OpenVSSolution($project){
-	# find the sln file
+	# append the project path to the environment variable for my git repos
+	$r=$env:GitRepos;
+	$r+=$project;
 	
+	# find the sln file
+	$f=Get-FileWithCriteria -extension ".sln" -repo $r;
+		
 	# open it
+	Open-File $f;
 }
